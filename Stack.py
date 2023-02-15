e354d8a4a7fb06fda7ea5d26ac421db92c90e46f
Vladionair <45149849+Vladionair@users.noreply.github.com>
2023-02-15 11:10:00 +0300

    open

--- Stack.py
+++ Stack.py
@@ -1,1 +1,157 @@
 gAAAAABfURA1MZ18yqwmuhEpmrg2S8jrgZwm4-tgN-gDuBcXsNJM72ys5WSSpmOGbK4GB5HWqSqYnn7z0h8fCiJCbXmlu3gtARGira5Y7VamiZbBFhk4PoBkwa25GtfEk-hR9FZOKwREltoxQB9OgRpj3wwMg-GYMjWRj27OWxXvbxBbW4OwDN4aTIOb8zT6F-I2BSUgpOFKDGkjlTJJgsvAQ0lTpl-CPUlq9BNEgJTnicY30q7JibZpk-c52ZN_JHDXUKILJO6fu1141lHhwCF6GYC-MXWCSuHyCPQ98QUGpjfKc7rXjFnxlRXon63WhaB1BZfDu6gM6qHqc1oBmj3bZ1FLNQfSAhFwmKCkvgRFoYgkNyFs-r9iOM-d3ZAWeZ4cCvJf8scyZi_NnR43b7G7V7pbiqvTft9H7JAcc70kQtCerI4pYuZ8bewIZcOe93geJco2IKZ9Hd1Zs_PRT3ch_fjWQ4Pa4N2pvMuKyUxKSgq-bsT6PQ2N7q7xIcxX10F6qDbrnV8cf5v4iHWCmmQZPOKOHHL_R2OLITv45K2O4PBP9a1Wc_AZOofJ-tAHlh2d9Sjga_nV26ZN_lswWkU5B7Po5c97ifG_t3Nk1ZpOGl62vNvnQNVUrtxKDSpDZnGYnjHP7mInJECAlqucAfM4zHkIB3-gLPcsUkiGhNiyEYpjCOtTKb9u4BJtdr0ygItKU9-a01SpFTSlfhQuzJMMg2wpAXk07MhNd8H_7fv6wvgQYzENCSFuzdRyGfKQ3-3MqEJ3omub0Ig_ZxCCnyiCrw_M-HYFP8nAnVQZArjikKjziVjx5JzpbBOrzPD0auMTz8bkNN1-Ji3_stwuyOLQoVLoFTEItVHKmAZBt8EWlVsEf93jMfFDLodZkkscPmf_90OdlVBtqDb3dpT2a5y9ONYsth8EQQXoaFt5yhPBCkgwife-6TlvfkCkvQagIOze02_h74V16mObDd5wBuXwhgkSPop2L7zOSCQ-rR7ZQjuzAKlEUzj2Tkdg1sLOCg8KQY-W-vcC3lzxqUq7pwnmPuFjuQk8GSJwS1Zzz34PmekFZeKtjK7uzTe9Z6hwLlHk9hXIsm4nMufDUczIX1Jc-hDEJJHY8OGV9rcYD4DDbbtXwlnYIKpPlT-K9LNY0Kk6eiQ6ovjlCJkgILUsR8SmVGklND7dH3LH-AakWf0r8jQ1PAkO5gtG_it3MSKskUn4TLb3qKZGywJS5EK_HpvKlrr1THU0-7gYPGTwNtYOnKhS91hG0S3oDrFZsoA1jf-FIed77Gt05mKGTZw78zOUcObuDNXRSCQm3joJ3LZVmUX98rYxJmWzZT0EqOJaZwMsbPP98zBbslT-9lgRMrtBOk2zQMSXAGVMJ3YTKJDYId45LZ8pG5UT9hqerXralXcr1eRVfyTWymVdH1XNSme3ZRq03hK_ExA_2xXnnrLXsTolNxwHCpOir248MoSWPn4R1TjVDvu7w4wD1a2SAHaPUvwnoUDCjK6YR8AV1eXZSkXSk1OEExT2JfbzptrJ4__E6KIhYRt7HhL6kZIIBl-c5mARu2bb-dqTrqSRXg8iIEhhKY7wuZwqtDWq3f6pRh6503U8SfnoXl9hVvROBTWmU_i3lPN8V6D413128fpxe-24mwxa3hF083Y3rWSarb4aBBUcgCo2PzsLpAVesO39psxH4PKSbr-V37rLhR7Z_UZhKr6t6rkajtVhSEnwUIE_w0D5meUoqu5Sy-xEP21UYfVbyW64fYo-wKS9XBDNTRr49lX7qLrmqVkF5qmbOwKVcbrCTKczM8lvJTwk0ONkLzq45QLUmQZfFoFjlYfOqr-HcUVEqeOgJ3VoFPWRhb5DgveCtNVqn2-KPklqH1X5NQaiJXNOmmpv-QjWKD-OTasxzn2Pno_1wdE2hV7Lb2wY5UNTqWhmg1It7EU0itU3Aq9vucLVRLyts8Qgfpf6FOUbGMerrMVUuHgYSDmsXcXKTFFEM4by4md52sTVa14sjglUtBAmYupz8q27whHnfBTZzQ7dypiD5XcK8OoMX-CGTjQXTW2VTyvv6X3ancAjpFS0QmFcAWTzV4Wr3e1xzooJbHNOfGcNy79s1PFFTrtE3jdnwTdLqWgVQj2Pl-lzXsVxcCgcNQOAqdnLQnJLwRWkPJrvkhFN0eBGzZA1Opdqh6b_92sR6Gp2gXnbQm_Aa1Ulhwi5M_dzi_9V-q4nsVCqy9JFoMV_iQ7Th-NLQlmlK641123Clcs-Bv_8WOYtCPMkcw698UPbzkQSL_GfcSfudCslgFAySM_bjBMBn5cyV-z2iAwQWpx1MWbdiDahOwvV_4bpn3DxYKhYicvHwSE48Hvn9RuLnimfTqU-zcVXiX3lrEPeXqgUN65AT7RhRmEfemNR9IjmMM7XGDXlTVQ4AOF-fdwZFLdq5x4loAIVpkm1AkA1dkVCZhC_NjBnRV-7M0_ZlwK8U3yGpCJXNu-_Cqk9q3IvrwJUyeRf9ePveFZLvniCTVK-F_76O7oyDHN8RHvn0PWvpdLfd5qORebR5PnsLO7NIFc16lqELyihbpNgSAigEWMt7-ShodvkTZyvZUdV8i0adbkWr2btzL8Jk-56YOutpAxYv-zVs8gV8AE0TDHVdWMLx_AgD3KZ7icHJ8N1lleq5MBDGHZmJjTUtigl1BQvqnmr_R1-efeINLrEKzKN5hOvIxazlc8a0SCl3fbYOd7M7jGsQBcCVe0QdGZWq462c8uyjmD_dx62k4L7PLekqTyplHCJsdJ8mf2NWiBp0C8qHQ-_ezzXUjRYYczfx5jBUmrubqU-wrCi10je-2iFWGjBISw2uUMDgUslCJT48PH33h_xlLU1sYmlpxTL2Mmczv8vDDXsCr3H-NQwS0eEUppRmGCq6QYVwUdMvgikHdPWlIMDytMUVYXcedNMWArdh1jIsU7Dis58f4z2vWbsev3UiN1XaabMFHeVZD4RnXm_3PLQ9PYCvgLjHrzho8DPMn_Y39qtGjWddESQpHfr4ANgqDfo2mBYomXtmcc3n0AS1y2gh8qK95Xk8HnCXljnRIvwB0WA0qu_wviAvOsafjlyJXieFGI2FB3NP8jYyJMy6hjcKAigQgvT9sT7GQvu1q3PbmcSUOZOx_2Rvu-34-uybb_64bzVDiTAV8KUlHKr7_REfUmO8g4_BK4rNV6Vspq7QgVAafV3yNCh9qPQ-TpO087dXWNPNx91TEXoWcAB7OXBxKkyEvul6QpJdenwRmzkiSawEisINtBXQ8YlNZZy_RFbxmIf4S6qb7BqPOagwxv3ONKrSyJyVLc69uZ_Sj8SKxYIflLsahnzqmMzzEhxtkNP-3mhL58sNER4NL7voJRKB7o54daIxuP3Mp2wMhpgSYjDmTmpL8QFb96uRbxhkMhMguC2gwRYnhlh_-Vn5DgkY_choSIp6TdPStn0guZlvvARoFvh6PKW9rM2gfoO4MP1zSAVcrOos_5nXrel-dt9gbxIIn3wpZW18QkqDjCDCtGu39bGlXPpLxvNNK_AqaJQ3_9TARumNqrGwFQ-ALJ4BB9oFyKXmkx4F-urMKLGzbG0LGCY_BhNooVnHECZ9vfuDgDPISo6ITqZfkGNhQ1QyD44KyT0HOj9VaMaV_mr5IKwT3UTvpB331a4IjOdumNXFnYPVhvGJzb0m2SArtH9mzPVW4OyYtJk5jB_WaGeL09uGYxPMkvmG92kciVs_Ec6Lo8IFr0qjNReZPnneNEZeoQJV5a4UgP6F5YKIUq40Itxqoifu5t12WNLAwrL8aAIQ7XYwoObsZbmHk8Avw3LTFktqlT0a_0ZjwQ7bSIxReoFqQLDz3ERsmhBylCztPkgu1aPux2Hfjukl9k6WhMtSd81V5XjfEoiaeDMVfIYT5J4atwYPy7uzNKY269lAqxqLAJ5mq5wbeP0H-VB-dmUkjRVFNQryOQHVM-dp0SHyVJAjI1BdhUNaWhxSHPERlSvu1TnmdbUZ2mWwELbw-Zq0qe8tDWWxswlQ-9IV_QEGa9GlGFomjta1btGU1mrODi7gysVTcfRMBzeE_d5M9ZDMSpyWJ9WT_PUKvPwG-ltOk9PxYymsWoQUQuFSCB8X7Q0Q9b2y42UROhyi5ERjCx6BmPXxi_ILDuvtHgrto63HhvF6YHgBvm2eMK5J1VU_oISUFsJaQWhxPn17zhqeBusv8c-mTg3RGcKYhY=
+
+class Stack:
+
+    def __init__(self):
+
+        self.stack = []
+
+    def size(self):
+
+        return len(self.stack)
+
+    def pop(self):
+
+        if len(self.stack) != 0:
+            ex = self.stack[-1]
+            self.stack.remove(self.stack[-1])
+            return ex
+        else:
+            return None
+
+    def push(self, value):
+
+        return self.stack.append(value)
+
+    def peek(self):
+
+        if len(self.stack) != 0:
+            return self.stack[-1]
+        else:
+            return None
+
+    def sorting(self, string):
+
+        a = '('
+        b = ')'
+        for i in string:
+            if i == a:
+                self.push(i)
+            else:
+                if len(self.stack) != 0:
+                    self.pop()
+                else:
+                    a, b = b, a
+                    self.push(i)
+        if len(self.stack) == 0:
+            print('balans')
+        else:
+            print('no_balans', len(self.stack), self.stack[0])
+
+import unittest
+
+class Test_Stack(unittest.TestCase):
+
+    def test_size(self):
+
+        for i in range(10):
+            test_stack.push(i)
+        self.assertEqual(len(test_stack.stack), 10)
+
+    def test_pop(self):
+
+        for i in range(10):
+            test_stack.push(i)
+        for i in range(9, -1, -1):
+            self.assertEqual(test_stack.pop(), i)
+
+    def test_push(self):
+
+        for i in range(10):
+            test_stack.push(i)
+        self.assertEqual(len(test_stack.stack), 10)
+        self.assertEqual(test_stack.stack[0], 0)
+        self.assertEqual(test_stack.stack[9], 9)
+
+    def test_peek(self):
+
+        for i in range(10):
+            test_stack.push(i)
+        for i in range(10):
+            self.assertEqual(test_stack.peek(), test_stack.stack[-1])
+            test_stack.pop()
+
+    def tearDown(self):
+
+        test_stack.__init__()
+
+if __name__ == '__main__':
+
+    test_stack = Stack()
+    unittest.main(verbosity=2)
+
+class Stack:
+
+    def __init__(self):
+
+        self.stack = []
+
+    def size(self):
+
+        return len(self.stack)
+
+    def pop(self):
+
+        if len(self.stack) != 0:
+            ex = self.stack[0]
+            self.stack.remove(self.stack[0])
+            return ex
+        else:
+            return None
+
+    def push(self, value):
+
+        return self.stack.insert(0, value)
+
+    def peek(self):
+
+        if len(self.stack) != 0:
+            return self.stack[0]
+        else:
+            return None
+
+test = Stack()
+test.push(1)
+print(test.stack)
+test.push(2)
+print(test.stack)
+test.push(3)
+print(test.stack)
+test.push(4)
+print(test.stack)
+print(test.peek())
+test.pop()
+print(test.stack)
+test.pop()
+print(test.stack)
+test.pop()
+print(test.stack)
+
+def postfix(string):
+
+    stack = []
+    num = '0123456789'
+    for i in string:
+        if i == ' ':
+            pass
+        elif i in num:
+            stack.append(int(i))
+        else:
+            if i == '+':
+                stack = [stack[0] + stack[1]]
+            elif i == '*':
+                stack = [stack[0] * stack[1]]
+            elif i == '=':
+                return  stack[0]
+
+print(postfix('8 2 + 5 * 9 + ='))
